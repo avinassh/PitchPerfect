9a123c835ec298b2303e408a824a34c0081782da
avi <avinashsajjan@gmail.com>
2015-03-09 14:38:46 +0530

    added code which records the audio.

--- Pitch Perfect/RecordSoundsViewController.swift
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class RecordSoundsViewController: UIViewController {
 
@@ -15,14 +16,43 @@ class RecordSoundsViewController: UIViewController {
     @IBOutlet weak var stopButton: UIButton!
     @IBOutlet weak var recordButton: UIButton!
     
+    var audioRecorder: AVAudioRecorder!
+    
     @IBAction func recordAudio(sender: UIButton) {
         recordingInProgress.hidden = false
         stopButton.hidden = false
         recordButton.enabled = false
+        
+        // code to record audio
+        // get the current directory path
+        let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+        var currentDateTime = NSDate()
+        var formatter = NSDateFormatter()
+        formatter.dateFormat = "ddMMyyyy-HHmmss"
+        // set the name of recorded audio file
+        var recordingName = formatter.stringFromDate(currentDateTime) + ".wav"
+        var pathArray = [dirPath, recordingName]
+        // set the file path
+        let filePath = NSURL.fileURLWithPathComponents(pathArray)
+        println(filePath!)
+        
+        // set up audio session
+        var session = AVAudioSession.sharedInstance()
+        session.setCategory(AVAudioSessionCategoryPlayAndRecord, error: nil)
+        
+        // initiliaze and prepare the recorder
+        audioRecorder = AVAudioRecorder(URL: filePath, settings: nil, error: nil)
+        audioRecorder.meteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
     }
     
     @IBAction func stopAudioRecording(sender: UIButton) {
         recordingInProgress.hidden = true
+        // stop audio record function and deactivate the session
+        audioRecorder.stop()
+        var audioSession = AVAudioSession.sharedInstance()
+        audioSession.setActive(false, error: nil)
     }
     
     
