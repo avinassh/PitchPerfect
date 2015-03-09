109625b6001e24c654e5fd26e8edb5a0744e45c2
avi <avinashsajjan@gmail.com>
2015-03-09 13:34:07 +0530

    added movie_quote file from class resources. now it is played whenever slow button is pressed (but not in slow motion)

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -7,16 +7,26 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
-
+    
+    var audioPlayer: AVAudioPlayer!
+    
     @IBAction func playSlowAudio(sender: UIButton) {
+        audioPlayer.play()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
+            var filePathURL = NSURL.fileURLWithPath(filePath)
+            audioPlayer = AVAudioPlayer(contentsOfURL: filePathURL, error: nil)
+        } else {
+            println("filepath is empty 😨")
+        }
     }
 
     override func didReceiveMemoryWarning() {
