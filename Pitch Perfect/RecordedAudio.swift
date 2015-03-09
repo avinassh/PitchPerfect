b71625c138d6c863f07f507cbf4a98073bb8f950
avi <avinashsajjan@gmail.com>
2015-03-09 14:54:28 +0530

    added RecordedAudio class, which acts as model for this app.

---
+++ Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by avi on 09/03/15.
+//  Copyright (c) 2015 avi. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
