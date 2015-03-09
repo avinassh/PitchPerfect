bcdd1142da20763a7f0f6806f0b392d26b375753
avi <avinashsajjan@gmail.com>
2015-03-09 14:25:36 +0530

    renamed the view controller to RecordSoundsViewController.

--- Pitch Perfect/RecordSoundsViewController.swift
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RecordSoundsViewController.swift
 //  Pitch Perfect
 //
 //  Created by avi on 09/03/15.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RecordSoundsViewController: UIViewController {
 
     
     @IBOutlet weak var recordingInProgress: UILabel!
